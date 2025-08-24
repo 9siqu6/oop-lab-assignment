c8c49ac55648ff0fadf2d442e5d7ac0b37c931b0
blablabla <lalala@mail.com>
2025-08-24 12:41:21 +0600

    implemented BankAccount with attributes and methods in C++

---
+++ src/BankAccount/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class BankAccount{
+private:
+    int accountNumber;
+    string accountHolderName;
+    double balance;
+
+public:
+    BankAccount(int accNum,string accName)
+    {
+        accountNumber=accNum;
+        accountHolderName=accName;
+        balance=0.0;
+    }
+    void deposit(double money)
+    {
+        balance+=money;
+    }
+    void withdraw(double money)
+    {
+        balance-=money;
+    }
+    double checkBalance()
+    {
+        cout<<balance<<endl;
+        return balance;
+    }
+};
+
+int main()
+{
+    return 0;
+}
